cb3407867e735882239ef0b7faa7822a4447bfc8
tharindua <tharindu.kumara@taprotechnologies.com>
2026-01-03 12:02:25 +0530

    init commit

---
+++ backend/oms-api/src/main/java/com/buddies/giftbox/oms/api/OmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.buddies.giftbox.oms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmsApiApplication.class, args);
+    }
+
+}
